f02e603269dd16908e2b34579b736ca0255353e0
yudakov21 <nikiiudakov@gmail.com>
2025-07-10 17:42:55 +0200

    Update fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from unittest.mock import AsyncMock
+from services.controller import ArtistController, TrackController
 
 
 @pytest.fixture
@@ -7,13 +8,32 @@ def mock_genius():
     return AsyncMock()
 
 @pytest.fixture
-def mock_spotify():
+def mock_parser():
     return AsyncMock()
 
 @pytest.fixture
-def mock_parser():
+def mock_spotify():
     return AsyncMock()
 
 @pytest.fixture
 def mock_db():
     return AsyncMock()
+
+
+@pytest.fixture
+def artist_controller(mock_genius, mock_parser, mock_spotify, mock_db):
+    return ArtistController(
+        genius=mock_genius,
+        genius_parser=mock_parser,
+        spotify=mock_spotify,
+        manager=mock_db
+    )
+
+@pytest.fixture
+def track_controller(mock_genius, mock_parser, mock_spotify, mock_db):
+    return TrackController(
+        genius=mock_genius,
+        genius_parser=mock_parser,
+        spotify=mock_spotify,
+        manager=mock_db
+    )
